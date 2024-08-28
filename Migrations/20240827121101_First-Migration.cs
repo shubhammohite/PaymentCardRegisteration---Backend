7852142430485895795c33e031e8c05492f85f7a
shubhammohite <shubhammohite9081@gmail.com>
2024-08-28 15:03:26 +0530

    We implement the CRUD Operation API's for PaymentCardRegisteration

---
+++ Migrations/20240827121101_First-Migration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentCardDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnername = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentCardDetailId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ Models/PaymentDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentCardDetailId { get; set; } 
+        public string CardOwnername { get; set; } = "";
+        public string CardNumber { get; set; } = "";
+        public string ExpirationDate { get; set; } = "";
+        public string SecurityCode { get; set; } = "";
+    }
+}
